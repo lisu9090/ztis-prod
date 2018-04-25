5d9706577142ffaeb39c7937faad1c0d1e8e738c
lisu9090 <lisu9090@gmail.com>
2018-04-25 11:25:00 +0200

    dodano komentarze w main()

--- src/main/java/pl/edu/agh/Main.java
+++ src/main/java/pl/edu/agh/Main.java
@@ -10,10 +10,11 @@ public class Main {
         System.out.println("Say w Szczebrzeszynie chrz¹szcz brzmi w trzcinie! :)");
         //PoissonGen p = new PoissonGen(6666.0);  
         
-        ProductionProcess process = new ProductionProcess(400.0, 1000.0, 270.0);
+        ProductionProcess process = new ProductionProcess(400.0, 1000.0, 270.0); //instancja processu z zainicjalizowanymi docelowymi parametrami
         if(args.length == 3)
             process.setTargetParams(Double.parseDouble(args[0]), Double.parseDouble(args[1]), Double.parseDouble(args[2]));
         
+        //Ustawaimy zakresy przedzialow losowania
         process.parameters.get(0).setGeneratorRange(60.0);
         process.parameters.get(1).setGeneratorRange(0.05);
         process.parameters.get(2).setGeneratorRange(100.0);
@@ -25,7 +26,7 @@ public class Main {
         
         //process.disableGenerator();
         try{
-           Double wjp = process.runProcess(1900.0, 2.0, 16000.0);
+           Double wjp = process.runProcess(1900.0, 2.0, 16000.0); //uruchom caly proces z domyslnymi wartosciami i zwroc wjp
            System. out.println("Process sucessfull! Obtained WJP = " + wjp);
         }
         catch(Exception e)
