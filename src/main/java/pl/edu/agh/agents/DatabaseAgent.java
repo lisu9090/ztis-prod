5a735ae99a6a422c16c9ac27f891a1f6e0345ca9
Wojciech Jędras <wjedras92@gmail.com>
2018-06-19 02:12:12 +0200

    Merge remote-tracking branch 'remotes/origin/ui' into agents
                             # Conflicts:
                             #	ProductionResult.json

--- src/main/java/pl/edu/agh/agents/DatabaseAgent.java
+++ src/main/java/pl/edu/agh/agents/DatabaseAgent.java
@@ -5,6 +5,12 @@ import jade.core.Agent;
 import jade.core.behaviours.CyclicBehaviour;
 import jade.lang.acl.ACLMessage;
 import jade.lang.acl.MessageTemplate;
+import pl.edu.agh.db.DBManager;
+import pl.edu.agh.parameter.ProcessJson;
+import pl.edu.agh.random.AgentMessages;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DatabaseAgent extends Agent {
 
@@ -19,13 +25,19 @@ public class DatabaseAgent extends Agent {
             public void action()
             {
                 MessageTemplate checkState = MessageTemplate.MatchPerformative(CHECK_AGENT);
-
+                MessageTemplate getDatabase = MessageTemplate.MatchPerformative(AgentMessages.GET_DATABASE_INSTANCE);
                 ACLMessage checkMsg = receive(checkState);
+                ACLMessage dbMsg = receive(getDatabase);
                 if (checkMsg!=null){
                     ACLMessage reply = new ACLMessage(CHECK_AGENT);
                     reply.setContent("success");
                     reply.addReceiver(new AID( args[0].toString(), AID.ISLOCALNAME));
                     send(reply);
+                }
+                else if(dbMsg!=null){
+                    List<ProcessJson> processes = DBManager.getINSTANCE().findAllProcesses();
+                    Object listProcesses = new ArrayList<ProcessJson>(processes);
+                    ACLMessage reply = new ACLMessage(AgentMessages.GET_DATABASE_INSTANCE);
                 }
                 block();
             }
