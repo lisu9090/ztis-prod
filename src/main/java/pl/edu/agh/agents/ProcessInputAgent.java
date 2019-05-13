ea5d8cad3662048fc511a4ed577cc6d447c324c1
oswin <uchwatm@gmail.com>
2019-05-13 22:33:01 +0000

    add naive learning

--- src/main/java/pl/edu/agh/agents/ProcessInputAgent.java
+++ src/main/java/pl/edu/agh/agents/ProcessInputAgent.java
@@ -1,14 +1,19 @@
 package pl.edu.agh.agents;
 
-import jade.core.AID;
-import jade.core.Agent;
-import jade.core.behaviours.CyclicBehaviour;
-import jade.lang.acl.ACLMessage;
-import jade.lang.acl.MessageTemplate;
+import org.apache.commons.math3.stat.regression.OLSMultipleLinearRegression;
 import org.json.JSONObject;
 
-public class ProcessInputAgent extends MLAgent {
+import java.util.ArrayList;
+import java.util.Iterator;
 
+public class ProcessInputAgent extends MLAgent {
+    private double best_temp = 1900;
+    private double best_mass = 1600;
+    private double best_volume = 2;
+    private JSONObject recieved;
+    private double regressionParameters[];
+
+    private ArrayList<Observation> observations = new ArrayList<>();
     @Override
     protected void loadRecentConfig() {
 
@@ -16,11 +21,71 @@ public class ProcessInputAgent extends MLAgent {
 
     @Override
     protected void learn() {
+        blockingSendReceiveForData(null);
+        parseDataAndFindCurrentBest(recieved);
+        System.out.println("Learned: mass "+best_mass+" volume: "+best_volume+" temp: "+best_temp);
+        learnRegession();
+    }
 
+    private void learnRegession(){
+        int num_observations = observations.size();
+        double [] y = new double[num_observations];
+        double [][] x = new double[num_observations][];
+
+        for(int i = 0; i<num_observations; i++){
+            Observation observation =  observations.get(i);
+            y[i] = observation.wjp;
+            x[i] = new double []{observation.in_temperature, observation.in_volume, observation.in_mass};
+        }
+
+        OLSMultipleLinearRegression regression = new OLSMultipleLinearRegression();
+        regression.newSampleData(y, x);
+
+        double[] regressionParameters = regression.estimateRegressionParameters();
+    }
+
+
+    private void parseDataAndFindCurrentBest(JSONObject jsonObject){
+        Iterator<String> keys = jsonObject.keys();
+
+        double min_wjp = Double.MAX_VALUE;
+        while(keys.hasNext()) {
+            String key = keys.next();
+            if (jsonObject.get(key) instanceof JSONObject) {
+                // do something with jsonObject here
+                System.out.println(key);
+                System.out.println(jsonObject.get(key));
+                JSONObject one = (JSONObject) jsonObject.get(key);
+                System.out.println(one);
+                System.out.println(one.get("wjp"));
+                double wjp = Double.parseDouble((String)one.get("wjp"));
+                JSONObject stage_1 = (JSONObject) one.get("stage_1");
+                try {
+                    double in_temperature = (double)(int) stage_1.get("in_temperature");
+                    double in_volume = (double)(int) stage_1.get("in_volume");
+                    double in_mass = (double)(int) stage_1.get("in_mass");
+                    observations.add(new Observation(in_temperature, in_volume, in_mass, wjp));
+                    if (wjp < min_wjp) {
+                        min_wjp = wjp;
+                        best_mass = in_mass;
+                        best_temp = in_temperature;
+                        best_volume = in_volume;
+                    }
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }
+        System.out.println(min_wjp);
     }
 
     @Override
     protected void receiveData(String data) {
+        //dostaję wynik w stringu
+        JSONObject jsonObject = new JSONObject(data);
+        recieved = jsonObject;
+        System.out.println(jsonObject);
+        //zapisać dane w tablicy i potem odczytać w learn
 
     }
 
@@ -29,15 +94,30 @@ public class ProcessInputAgent extends MLAgent {
         JSONObject jsonToSend = new JSONObject(params);
         
         if(jsonToSend.getString("user_temperature").equals(""))
-            jsonToSend.put("temperature", 1900.0);
+            jsonToSend.put("temperature", best_temp);
         
         if(jsonToSend.getString("user_mass").equals(""))
-            jsonToSend.put("mass", 16000.0);
+            jsonToSend.put("mass", best_mass);
         
         if(jsonToSend.getString("user_volume").equals(""))
-            jsonToSend.put("volume", 2.0);
-        
+            jsonToSend.put("volume", best_volume);
+
         return jsonToSend;
     }
     
+}
+
+class Observation{
+    public double in_temperature;
+    public double in_volume;
+    public double in_mass;
+    public double wjp;
+
+    Observation(double in_temperature,double in_volume, double in_mass, double wjp){
+        this.in_temperature = in_temperature;
+        this.in_volume = in_volume;
+        this.in_mass = in_mass;
+        this.wjp = wjp;
+    }
+
 }
